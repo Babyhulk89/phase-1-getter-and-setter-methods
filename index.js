c4566417ff39b62383aefaf9d7d818d4457d6360
Babyhulk89 <131919459+Babyhulk89@users.noreply.github.com>
2023-12-10 17:08:04 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Add your Circle class here
+class Circle {
+    #radius;
+  
+    constructor(radius) {
+      this.#radius = radius;
+    }
+  
+    // Getter method for diameter
+    get diameter() {
+      return this.#radius * 2;
+    }
+  
+    // Getter method for circumference
+    get circumference() {
+      return Math.PI * this.diameter;
+    }
+  
+    // Getter method for area
+    get area() {
+      return Math.PI * Math.pow(this.#radius, 2);
+    }
+  
+    // Setter method for diameter
+    set diameter(value) {
+      this.#radius = value / 2;
+    }
+  
+    // Setter method for circumference
+    set circumference(value) {
+      this.#radius = value / (2 * Math.PI);
+    }
+  
+    // Setter method for area
+    set area(value) {
+      this.#radius = Math.sqrt(value / Math.PI);
+    }
+  
+    // Getter method for radius
+    get radius() {
+      return this.#radius;
+    }
+  }
+  
+  // Export the Circle class if needed
+  module.exports = Circle;
+  
